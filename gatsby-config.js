4fce778c4ddb9e0d2d57387b31f7863811691ef9
Vladimir Zhelvis <war1lok@yandex.ru>
2019-11-20 03:58:13 +0300

    fix robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        policy: [{ userAgent: '*', allow: '/' }],
+        policy: [{ userAgent: '*', disallow: '/' }],
       },
     },
   ],
