368314758e504ac26ebdec4946feb8bf3a5249b9
Vladimir Zhelvis <zhelvisvladimir@gmail.com>
2020-12-04 08:33:04 +0300

    Updates

--- sw.js
+++ sw.js
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "65dbf6451fa963c441b358e5cf4c557a"
+    "revision": "8f02dff2bbbe24fc0544d8a93e6a4ace"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
